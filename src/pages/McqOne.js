2e4bcab9750b25f93b4038ad307acb590c8d1e91
Quillyofficial <qisun724@gmail.com>
2025-02-07 14:12:24 -0800

    Finished mcqOne

--- src/pages/McqOne.js
+++ src/pages/McqOne.js
@@ -1,9 +1,93 @@
 import React from 'react'
+import yawn from '../images/yawn.PNG'
+import { Typography } from '@mui/material'
+import { FormHelperText } from '@mui/material'
+import { Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
-function mcqOne() {
+function McqOne() {
+  const navigate = useNavigate();
+  function handleSubmit(e) {  
+    e.preventDefault();      
+    navigate('/mcqTwo');
+  }
+
   return (
-    <div>mcqOne</div>
+    <div>
+
+    {/* Container for body of the page */}
+    <div class='container'> 
+      
+    <div style={{
+      position:'absolute', 
+      top:'160px',
+      bottom:'120px',
+      display:'flex',
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center'}}>
+
+      {/* Container for mcq image */}
+      <div style={{
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        justifyContent:'center'
+        }}>
+        <img class="mcqImage" src={yawn} alt="yawningImage"/>
+      </div>
+
+      {/* Container for text and buttons */}
+      <div style={{
+        position:'absolute', 
+        top:'90px',
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        justifyContent:'center'
+        }}>
+        
+        <Typography variant='h5' sx={{ textAlign: 'center', paddingBottom:'25px'}}>
+          That long lecture just ended <br />
+          ...what are you up to?
+        </Typography>
+
+        <FormHelperText sx={{ textAlign: 'center'}}>
+          Please choose an option
+        </FormHelperText>
+
+        <Button class='mcqButton'> 📖 Relaxing at home with a book or movie. </Button>
+        <Button class='mcqButton'> 👯 Going on a hot girl walk with friends. </Button>
+        <Button class='mcqButton'> 🎶 Attending a concert or festival. </Button>
+        <Button class='mcqButton'> 🏋️‍♀️ Sweating out the stress. </Button>
+
+        <Button onClick={handleSubmit}
+          sx={{
+            backgroundColor: 'rgba(232, 226, 237, 1)',
+            color: 'black',
+            fontWeight: 'bold',
+            borderRadius: '20px',
+            width: '400px',
+            height: '50px',
+            border: 1,
+            top: '30px'}}
+        > Continue ➤ </Button>
+        
+      </div>
+
+    </div>
+    </div>
+      <FormHelperText sx={{
+        textAlign: 'center',
+        position: 'absolute',
+        bottom: '0', 
+        width: '100%'
+        }}>
+        www.myquilly.com Terms of Use
+      </FormHelperText>
+
+    </div>
   )
 }
 
-export default mcqOne
+export default McqOne
