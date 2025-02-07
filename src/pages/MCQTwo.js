465ada29fbaef178b38f754843be037cedc247ab
Quillyofficial <qisun724@gmail.com>
2025-02-07 15:55:01 -0800

    Finished mcqTwo

--- src/pages/MCQTwo.js
+++ src/pages/MCQTwo.js
@@ -7,8 +7,101 @@ import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 
 function MCQTwo() {
+  const navigate = useNavigate();
+  const [selected, setSelected] = useState(0);
+  const options = [
+    {id: 1, emoji: "😕", text:"Not at all. I know what I like."},
+    {id: 2, emoji: "💆‍♀️", text:"I like exploring within my comfort zone."},
+    {id: 3, emoji: "🙆‍♀️", text:"I'll try something once."},
+    {id: 4, emoji: "🎢", text:"The amazing race called me."}
+  ];
+
+  function handleSubmit(e) {  
+    e.preventDefault();      
+    navigate('/mcqThree');
+  }
+
   return (
-    <div>mcqTwo</div>
+    <div>
+
+    {/* Container for body of the page */}
+    <div class='container'> 
+      
+    <div style={{
+      position:'absolute', 
+      top:'180px',
+      bottom:'100px',
+      display:'flex',
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center'}}>
+
+      {/* Container for mcq image */}
+      <div style={{
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        justifyContent:'center',
+        marginBottom:'40px'
+        }}>
+        <img class="mcqImage" src={camp} alt="campImage"/>
+      </div>
+
+      {/* Container for text and buttons */}
+      <div style={{
+        position:'absolute', 
+        top:'110px',
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        justifyContent:'center'
+        }}>
+        
+        <Typography variant='h5' sx={{ textAlign: 'center', paddingBottom:'25px'}}>
+          How down for an adventure are you?
+        </Typography>
+
+        <FormHelperText sx={{ textAlign: 'center'}}>
+          Please choose an option
+        </FormHelperText>
+
+        {/* Options */}
+        { options.map((option) => (
+          <Button 
+            class={`mcqButton ${selected === option.id ? "option-selected" : "mcqButton"}`}
+            key={option.id}
+            onClick={()=> setSelected(option.id)}
+            >
+            <span>{option.emoji}</span> {option.text}
+          </Button>
+        ))}
+
+        <Button onClick={handleSubmit}
+          sx={{
+            backgroundColor: 'rgba(232, 226, 237, 1)',
+            color: 'black',
+            fontWeight: 'bold',
+            borderRadius: '20px',
+            width: '400px',
+            height: '50px',
+            border: 1,
+            top: '30px'}}
+        > Continue ➤ </Button>
+        
+      </div>
+
+    </div>
+    </div>
+      <FormHelperText sx={{
+        textAlign: 'center',
+        position: 'absolute',
+        bottom: '0', 
+        width: '100%'
+        }}>
+        www.myquilly.com Terms of Use
+      </FormHelperText>
+
+    </div>
   )
 }
 
