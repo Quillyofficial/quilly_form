2bf8c26dff6999e28eb4da2c061ed011b0fcc9ef
hadar <hadar@streamlinesocial.com>
2025-02-14 08:35:48 +0200

    updated packages

--- src/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [], // Add any external image domains if needed
-  },
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
