8857d3f33b2a1da2d6ea3562a110d629d2c4af51
hadar <hadar@streamlinesocial.com>
2025-02-15 09:27:30 +0200

    added next config

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        zlib: require.resolve("browserify-zlib"),
+      };
+    }
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
