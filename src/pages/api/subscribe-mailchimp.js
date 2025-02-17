c2efad61d49947421ad288aa7d824acc549cbfca
hadar <hadar@streamlinesocial.com>
2025-02-17 15:21:34 -0800

    added debug code to the mailchimp calls

--- src/pages/api/subscribe-mailchimp.js
+++ src/pages/api/subscribe-mailchimp.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  //try {
+  try {
     // Check if all required environment variables are present
     // if (!process.env.REACT_APP_MAILCHIMP_API_KEY) {
     //  throw new Error('MAILCHIMP_API_KEY is missing');
